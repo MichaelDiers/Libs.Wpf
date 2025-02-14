84f0a10a71767f5faca31918e50a2da0b2c403a1
MichaelDiers <md@sreid.de>
2025-02-14 21:48:35 +0100

    add base view models and tests

---
+++ Libs.Wpf.Tests/ViewModels/ValidatorViewModelBaseTests.cs
@@ -0,0 +1,202 @@
+﻿namespace Libs.Wpf.Tests.ViewModels;
+
+using System.ComponentModel;
+using Libs.Wpf.ViewModels;
+
+/// <summary>
+///     Tests for <see cref="ValidatorViewModelBase" />.
+/// </summary>
+public class ValidatorViewModelBaseTests : ValidatorViewModelBase
+{
+    private const string ErrorProperty = nameof(ValidatorViewModelBaseTests.ErrorProperty);
+    private const string ErrorValue = nameof(ValidatorViewModelBaseTests.ErrorValue);
+
+    [Fact]
+    public void ErrorsChangedTest()
+    {
+        var called = false;
+
+        void HandleErrorsChanged(object? sender, DataErrorsChangedEventArgs e)
+        {
+            Assert.Equal(
+                this,
+                sender);
+            Assert.Equal(
+                e.PropertyName,
+                ValidatorViewModelBaseTests.ErrorProperty);
+            called = true;
+        }
+
+        this.ErrorsChanged += HandleErrorsChanged;
+
+        this.SetError(
+            ValidatorViewModelBaseTests.ErrorValue,
+            ValidatorViewModelBaseTests.ErrorProperty);
+
+        Assert.True(called);
+
+        called = false;
+
+        this.SetErrors(
+            [ValidatorViewModelBaseTests.ErrorValue],
+            ValidatorViewModelBaseTests.ErrorProperty);
+
+        Assert.True(called);
+
+        this.ErrorsChanged -= HandleErrorsChanged;
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(ValidatorViewModelBaseTests.ErrorProperty)]
+    public void GetErrorsTest(string? propertyName)
+    {
+        this.GetErrors(
+            propertyName,
+            []);
+
+        this.SetError(
+            ValidatorViewModelBaseTests.ErrorValue,
+            ValidatorViewModelBaseTests.ErrorProperty);
+        this.GetErrors(
+            propertyName,
+            [ValidatorViewModelBaseTests.ErrorValue]);
+
+        this.SetErrors(
+            [ValidatorViewModelBaseTests.ErrorValue, ValidatorViewModelBaseTests.ErrorValue],
+            ValidatorViewModelBaseTests.ErrorProperty);
+        this.GetErrors(
+            propertyName,
+            [ValidatorViewModelBaseTests.ErrorValue, ValidatorViewModelBaseTests.ErrorValue]);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void GetMultipleErrorsTest(string? propertyName)
+    {
+        var errors = new List<string>();
+        for (var i = 1; i < 10; ++i)
+        {
+            var propErrors = Enumerable.Range(
+                    0,
+                    i)
+                .Select(j => $"error_{j}")
+                .ToArray();
+            this.SetErrors(
+                propErrors,
+                $"property_{i}");
+            errors.AddRange(propErrors);
+        }
+
+        this.GetErrors(
+            propertyName,
+            errors.ToArray());
+    }
+
+    [Fact]
+    public void HasErrorsTest()
+    {
+        Assert.False(this.HasErrors);
+
+        this.SetError(
+            ValidatorViewModelBaseTests.ErrorValue,
+            ValidatorViewModelBaseTests.ErrorProperty);
+
+        Assert.True(this.HasErrors);
+
+        this.ResetErrors(ValidatorViewModelBaseTests.ErrorProperty);
+
+        Assert.False(this.HasErrors);
+
+        this.SetErrors(
+            [ValidatorViewModelBaseTests.ErrorValue],
+            ValidatorViewModelBaseTests.ErrorProperty);
+
+        Assert.True(this.HasErrors);
+
+        this.ResetErrors(ValidatorViewModelBaseTests.ErrorProperty);
+
+        Assert.False(this.HasErrors);
+    }
+
+    [Fact]
+    public void ResetErrorsTest()
+    {
+        Assert.False(this.HasErrors);
+
+        this.ResetErrors(ValidatorViewModelBaseTests.ErrorProperty);
+        Assert.False(this.HasErrors);
+
+        this.SetError(
+            ValidatorViewModelBaseTests.ErrorValue,
+            ValidatorViewModelBaseTests.ErrorProperty);
+        Assert.True(this.HasErrors);
+
+        this.ResetErrors($"{ValidatorViewModelBaseTests.ErrorProperty}x");
+        Assert.True(this.HasErrors);
+
+        this.ResetErrors($"{ValidatorViewModelBaseTests.ErrorProperty}");
+        Assert.False(this.HasErrors);
+    }
+
+    [Fact]
+    public void SetErrorsTest()
+    {
+        Assert.False(this.HasErrors);
+        for (var i = 0; i < 10; ++i)
+        {
+            this.SetErrors(
+                Enumerable.Range(
+                        0,
+                        i + 1)
+                    .Select(j => $"{ValidatorViewModelBaseTests.ErrorValue}_{i}_{j}")
+                    .ToArray(),
+                $"{ValidatorViewModelBaseTests.ErrorProperty}_{i}");
+        }
+
+        for (var i = 0; i < 10; ++i)
+        {
+            this.GetErrors(
+                $"{ValidatorViewModelBaseTests.ErrorProperty}_{i}",
+                Enumerable.Range(
+                        0,
+                        i + 1)
+                    .Select(j => $"{ValidatorViewModelBaseTests.ErrorValue}_{i}_{j}")
+                    .ToArray());
+        }
+    }
+
+    [Fact]
+    public void SetErrorTest()
+    {
+        Assert.False(this.HasErrors);
+        for (var i = 0; i < 10; ++i)
+        {
+            this.SetError(
+                $"{ValidatorViewModelBaseTests.ErrorValue}{i}",
+                $"{ValidatorViewModelBaseTests.ErrorProperty}{i}");
+        }
+
+        for (var i = 0; i < 10; ++i)
+        {
+            this.GetErrors(
+                $"{ValidatorViewModelBaseTests.ErrorProperty}{i}",
+                [$"{ValidatorViewModelBaseTests.ErrorValue}{i}"]);
+        }
+    }
+
+    private void GetErrors(string? propertyName, string[] expectedErrors)
+    {
+        var actualErrors = new List<string>();
+        foreach (string error in this.GetErrors(propertyName))
+        {
+            actualErrors.Add(error);
+        }
+
+        Assert.Equal(
+            expectedErrors,
+            actualErrors);
+    }
+}
---
+++ Libs.Wpf.Tests/ViewModels/ViewModelBaseTests.cs
@@ -0,0 +1,66 @@
+﻿namespace Libs.Wpf.Tests.ViewModels;
+
+using System.ComponentModel;
+using Libs.Wpf.ViewModels;
+
+/// <summary>
+///     Tests of <see cref="ViewModelBase" />.
+/// </summary>
+public class ViewModelBaseTests : ViewModelBase
+{
+    /// <summary>
+    ///     The test property.
+    /// </summary>
+    private string testProperty = string.Empty;
+
+    /// <summary>
+    ///     Gets or sets the testProperty.
+    /// </summary>
+    public string TestProperty
+    {
+        get => this.testProperty;
+        set =>
+            this.SetField(
+                ref this.testProperty,
+                value);
+    }
+
+    [Fact]
+    public void PropertyChanged_NotRaisedIfPropertyNotChanged()
+    {
+        void HandlePropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            Assert.Fail("should not occur");
+        }
+
+        this.TestProperty = "foo";
+
+        this.PropertyChanged += HandlePropertyChanged;
+
+        this.TestProperty = "foo";
+
+        this.PropertyChanged -= HandlePropertyChanged;
+    }
+
+    [Fact]
+    public void PropertyChanged_RaisedIfPropertyChanged()
+    {
+        var propertyChangedRaised = false;
+
+        void HandlePropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            propertyChangedRaised = true;
+            Assert.Equal(
+                nameof(ViewModelBaseTests.TestProperty),
+                e.PropertyName);
+        }
+
+        this.PropertyChanged += HandlePropertyChanged;
+
+        this.TestProperty = "foo";
+
+        this.PropertyChanged -= HandlePropertyChanged;
+
+        Assert.True(propertyChangedRaised);
+    }
+}
