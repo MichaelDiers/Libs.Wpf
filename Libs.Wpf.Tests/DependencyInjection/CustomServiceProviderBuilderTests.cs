4368120ee7596982082f39ac5c7b4bcef6877952
MichaelDiers <md@sreid.de>
2025-02-15 15:19:58 +0100

    add dependency injection

---
+++ Libs.Wpf.Tests/DependencyInjection/CustomServiceProviderBuilderTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Libs.Wpf.Tests.DependencyInjection;
+
+using Libs.Wpf.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+///     Tests of <see cref="CustomServiceProviderBuilder" />.
+/// </summary>
+public class CustomServiceProviderBuilderTests
+{
+    [Fact]
+    public void Build_SucceedsAddingDependencies()
+    {
+        var provider = CustomServiceProviderBuilder.Build(services => services.AddSingleton<IFoo, Foo>());
+
+        var foo = provider.GetRequiredService<IFoo>();
+
+        Assert.IsAssignableFrom<IFoo>(foo);
+    }
+
+    [Fact]
+    public void Build_SucceedsWithoutDependencies()
+    {
+        _ = CustomServiceProviderBuilder.Build();
+    }
+
+    private interface IFoo;
+
+    private class Foo : IFoo;
+}
