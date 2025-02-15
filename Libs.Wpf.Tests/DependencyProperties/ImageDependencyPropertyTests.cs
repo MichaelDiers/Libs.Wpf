9bb2e3568752e169b78b7c0810c0d888f407f87f
MichaelDiers <md@sreid.de>
2025-02-15 16:40:33 +0100

    add dependency properties and tests

---
+++ Libs.Wpf.Tests/DependencyProperties/ImageDependencyPropertyTests.cs
@@ -0,0 +1,48 @@
+﻿namespace Libs.Wpf.Tests.DependencyProperties;
+
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Libs.Wpf.DependencyProperties;
+
+/// <summary>
+///     Tests of <see cref="ImageDependencyProperty" />
+/// </summary>
+public class ImageDependencyPropertyTests
+{
+    [Fact]
+    public void GetImage()
+    {
+        var dependencyObject = new DependencyObject();
+        var expected = new BitmapImage();
+
+        ImageDependencyProperty.SetImage(
+            dependencyObject,
+            expected);
+
+        var actual = ImageDependencyProperty.GetImage(dependencyObject);
+
+        Assert.Equal(
+            expected,
+            actual);
+    }
+
+    [Fact]
+    public void GetImage_ShouldReturnNull_WhenNotSet()
+    {
+        var dependencyObject = new DependencyObject();
+
+        var actual = ImageDependencyProperty.GetImage(dependencyObject);
+
+        Assert.Null(actual);
+    }
+
+    [Fact]
+    public void SetImage()
+    {
+        var dependencyObject = new DependencyObject();
+
+        ImageDependencyProperty.SetImage(
+            dependencyObject,
+            new BitmapImage());
+    }
+}
---
+++ Libs.Wpf.Tests/DependencyProperties/Watermark/WatermarkDependencyPropertyTests.cs
@@ -0,0 +1,47 @@
+﻿namespace Libs.Wpf.Tests.DependencyProperties.Watermark;
+
+using System.Windows;
+using Libs.Wpf.DependencyProperties.Watermark;
+
+/// <summary>
+///     Tests of <see cref="WatermarkDependencyProperty" />
+/// </summary>
+public class WatermarkDependencyPropertyTests
+{
+    [Fact]
+    public void GetWatermark()
+    {
+        var dependencyObject = new DependencyObject();
+        const string expected = "watermark";
+
+        WatermarkDependencyProperty.SetWatermark(
+            dependencyObject,
+            expected);
+
+        var actual = WatermarkDependencyProperty.GetWatermark(dependencyObject);
+
+        Assert.Equal(
+            expected,
+            actual);
+    }
+
+    [Fact]
+    public void GetWatermark_ShouldReturnNull_WhenNotSet()
+    {
+        var dependencyObject = new DependencyObject();
+
+        var actual = WatermarkDependencyProperty.GetWatermark(dependencyObject);
+
+        Assert.Null(actual);
+    }
+
+    [Fact]
+    public void SetWatermark_UsingNoControl()
+    {
+        var dependencyObject = new DependencyObject();
+
+        WatermarkDependencyProperty.SetWatermark(
+            dependencyObject,
+            "watermark");
+    }
+}
