ebbd5bfa37c882a81ac5afb29bad2239d521b3c3
MichaelDiers <md@sreid.de>
2025-02-15 14:51:51 +0100

    add configuration reader and tests

---
+++ Libs.Wpf.Tests/Configuration/CustomConfigurationBuilderTests.cs
@@ -0,0 +1,77 @@
+﻿namespace Libs.Wpf.Tests.Configuration;
+
+using Libs.Wpf.Configuration;
+
+/// <summary>
+///     Tests of <see cref="CustomConfigurationBuilder" />.
+/// </summary>
+public class CustomConfigurationBuilderTests
+{
+    [Fact]
+    public void GetConfiguration_Succeeds()
+    {
+        var configuration = CustomConfigurationBuilder.GetConfiguration<CustomConfig>();
+
+        Assert.Equal(
+            "Bar",
+            configuration.Foo);
+    }
+
+    [Fact]
+    public void GetConfiguration_ThrowsDirectoryNotFoundException_WhenAppSettingsDirectoryDoesNotExist()
+    {
+        Assert.Throws<DirectoryNotFoundException>(
+            () => CustomConfigurationBuilder.GetConfiguration<CustomConfig>(Guid.NewGuid().ToString()));
+    }
+
+    [Theory]
+    [InlineData(
+        null,
+        typeof(ArgumentNullException))]
+    [InlineData(
+        "",
+        typeof(ArgumentException))]
+    [InlineData(
+        "  ",
+        typeof(ArgumentException))]
+    public void GetConfiguration_ThrowsException_WhenAppSettingsFileNameIsNotSet(
+        string appSettingsFileName,
+        Type expectedExceptionType
+    )
+    {
+        Assert.Throws(
+            expectedExceptionType,
+            () => CustomConfigurationBuilder.GetConfiguration<CustomConfig>(
+                null,
+                appSettingsFileName));
+    }
+
+    [Fact]
+    public void GetConfiguration_ThrowsFileNotFoundExceptionException_WhenAppSettingsFileDoesNotExist()
+    {
+        Assert.Throws<FileNotFoundException>(
+            () => CustomConfigurationBuilder.GetConfiguration<CustomConfig>(
+                null,
+                Guid.NewGuid().ToString()));
+    }
+
+    [Fact]
+    public void GetConfiguration_ThrowsInvalidOperationException_WhenConfigurationDoNotMatch()
+    {
+        Assert.Throws<InvalidOperationException>(
+            () => CustomConfigurationBuilder.GetConfiguration<AnotherCustomConfig>());
+    }
+
+    // ReSharper disable once ClassNeverInstantiated.Local
+    private class CustomConfig(string foo)
+    {
+        public string Foo => foo;
+    }
+
+    // ReSharper disable once ClassNeverInstantiated.Local
+    private class AnotherCustomConfig(string fooBar)
+    {
+        // ReSharper disable once UnusedMember.Local
+        public string FooBar => fooBar;
+    }
+}
---
+++ Libs.Wpf/Configuration/CustomConfigurationBuilder.cs
@@ -0,0 +1,55 @@
+﻿namespace Libs.Wpf.Configuration;
+
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+/// <summary>
+///     Build the configuration from an <c>appsettings.json</c> file.
+/// </summary>
+public static class CustomConfigurationBuilder
+{
+    /// <summary>
+    ///     The default name of the settings file.
+    /// </summary>
+    private const string AppSettingsJson = "appsettings.json";
+
+    /// <summary>
+    ///     Read the application settings file and get the specified configuration.
+    /// </summary>
+    /// <typeparam name="T">The type of the configuration.</typeparam>
+    /// <param name="appSettingsDirectory">
+    ///     The directory that the <paramref name="appSettingsFileName" /> contains. Defaults to
+    ///     <see cref="Directory.GetCurrentDirectory" />.
+    /// </param>
+    /// <param name="appSettingsFileName">
+    ///     The name of the application settings file. Defaults to <see cref="AppSettingsJson" />
+    ///     .
+    /// </param>
+    /// <returns>The requested configuration of type <typeparamref name="T" />.</returns>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="appSettingsFileName" /> is null or whitespace.</exception>
+    /// <exception cref="InvalidOperationException">Thrown if the configuration initialization fails.</exception>
+    public static T GetConfiguration<T>(
+        string? appSettingsDirectory = null,
+        string? appSettingsFileName = CustomConfigurationBuilder.AppSettingsJson
+    )
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(appSettingsFileName);
+
+        var basePath = appSettingsDirectory ?? Directory.GetCurrentDirectory();
+
+        if (!Directory.Exists(basePath))
+        {
+            throw new DirectoryNotFoundException(basePath);
+        }
+
+        var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(
+                appSettingsFileName,
+                false,
+                false);
+
+        var configuration = builder.Build();
+
+        return configuration.Get<T>() ?? throw new InvalidOperationException($"Cannot initialize {typeof(T).Name}.");
+    }
+}
