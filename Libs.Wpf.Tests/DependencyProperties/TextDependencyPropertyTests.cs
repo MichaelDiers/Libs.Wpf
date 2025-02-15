29b8ed584cdea8d482561df89e913ca302a20410
MichaelDiers <md@sreid.de>
2025-02-16 00:57:00 +0100

    add text dependency property

---
+++ Libs.Wpf.Tests/DependencyProperties/TextDependencyPropertyTests.cs
@@ -0,0 +1,47 @@
+﻿namespace Libs.Wpf.Tests.DependencyProperties;
+
+using System.Windows;
+using Libs.Wpf.DependencyProperties;
+
+/// <summary>
+///     Tests of <see cref="TextDependencyProperty" />
+/// </summary>
+public class TextDependencyPropertyTests
+{
+    [Fact]
+    public void GetText()
+    {
+        var dependencyObject = new DependencyObject();
+        var expected = "text";
+
+        TextDependencyProperty.SetText(
+            dependencyObject,
+            expected);
+
+        var actual = TextDependencyProperty.GetText(dependencyObject);
+
+        Assert.Equal(
+            expected,
+            actual);
+    }
+
+    [Fact]
+    public void GetText_ShouldReturnNull_WhenNotSet()
+    {
+        var dependencyObject = new DependencyObject();
+
+        var actual = TextDependencyProperty.GetText(dependencyObject);
+
+        Assert.Null(actual);
+    }
+
+    [Fact]
+    public void SetText()
+    {
+        var dependencyObject = new DependencyObject();
+
+        TextDependencyProperty.SetText(
+            dependencyObject,
+            "text");
+    }
+}
---
+++ Libs.Wpf/DependencyProperties/TextDependencyProperty.cs
@@ -0,0 +1,40 @@
+﻿namespace Libs.Wpf.DependencyProperties;
+
+using System.Windows;
+
+/// <summary>
+///     An attached <see cref="DependencyProperty" /> that adds the text property.
+/// </summary>
+public static class TextDependencyProperty
+{
+    /// <summary>
+    ///     The text <see cref="DependencyProperty" />.
+    /// </summary>
+    public static readonly DependencyProperty TextProperty = DependencyProperty.RegisterAttached(
+        nameof(TextDependencyProperty.TextProperty)[..^8],
+        typeof(string),
+        typeof(TextDependencyProperty),
+        new PropertyMetadata(default(string)));
+
+    /// <summary>
+    ///     Gets the value of the text <see cref="DependencyProperty" />.
+    /// </summary>
+    /// <param name="element">The element to that the <see cref="DependencyProperty" /> is attached to.</param>
+    /// <returns>The value of the <see cref="DependencyProperty" />.</returns>
+    public static string GetText(DependencyObject element)
+    {
+        return (string) element.GetValue(TextDependencyProperty.TextProperty);
+    }
+
+    /// <summary>
+    ///     Sets the value of the text <see cref="DependencyProperty" />.
+    /// </summary>
+    /// <param name="element">The element to that the <see cref="DependencyProperty" /> is attached to.</param>
+    /// <param name="value">The new value of the <see cref="DependencyProperty" />.</param>
+    public static void SetText(DependencyObject element, string value)
+    {
+        element.SetValue(
+            TextDependencyProperty.TextProperty,
+            value);
+    }
+}
