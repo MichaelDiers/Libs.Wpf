3ac47016387801c88b31105bde31965eafd69dcb
MichaelDiers <md@sreid.de>
2025-02-15 17:31:07 +0100

    add control template for buttons

---
+++ Libs.Wpf.Tests/DependencyProperties/CornerRadiusDependencyPropertyTests.cs
@@ -0,0 +1,58 @@
+﻿namespace Libs.Wpf.Tests.DependencyProperties;
+
+using System.Windows;
+using Libs.Wpf.DependencyProperties;
+
+/// <summary>
+///     Tests of <see cref="CornerRadiusDependencyProperty" />
+/// </summary>
+public class CornerRadiusDependencyPropertyTests
+{
+    [Fact]
+    public void GetCornerRadius()
+    {
+        var dependencyObject = new DependencyObject();
+        var expected = new CornerRadius();
+
+        CornerRadiusDependencyProperty.SetCornerRadius(
+            dependencyObject,
+            expected);
+
+        var actual = CornerRadiusDependencyProperty.GetCornerRadius(dependencyObject);
+
+        Assert.Equal(
+            expected,
+            actual);
+    }
+
+    [Fact]
+    public void GetCornerRadius_ShouldReturnNull_WhenNotSet()
+    {
+        var dependencyObject = new DependencyObject();
+
+        var actual = CornerRadiusDependencyProperty.GetCornerRadius(dependencyObject);
+
+        Assert.Equal(
+            5,
+            actual.BottomLeft);
+        Assert.Equal(
+            5,
+            actual.BottomRight);
+        Assert.Equal(
+            5,
+            actual.TopLeft);
+        Assert.Equal(
+            5,
+            actual.TopRight);
+    }
+
+    [Fact]
+    public void SetCornerRadius()
+    {
+        var dependencyObject = new DependencyObject();
+
+        CornerRadiusDependencyProperty.SetCornerRadius(
+            dependencyObject,
+            new CornerRadius());
+    }
+}
---
+++ Libs.Wpf/DependencyProperties/CornerRadiusDependencyProperty.cs
@@ -0,0 +1,45 @@
+﻿namespace Libs.Wpf.DependencyProperties;
+
+using System.Windows;
+
+/// <summary>
+///     An attached <see cref="DependencyProperty" /> that adds the corner radius property.
+/// </summary>
+public static class CornerRadiusDependencyProperty
+{
+    /// <summary>
+    ///     The default value of the corner radius.
+    /// </summary>
+    private const double DefaultCornerRadius = 5;
+
+    /// <summary>
+    ///     The corner radius <see cref="DependencyProperty" />.
+    /// </summary>
+    public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.RegisterAttached(
+        nameof(CornerRadiusDependencyProperty.CornerRadiusProperty)[..^8],
+        typeof(CornerRadius),
+        typeof(CornerRadiusDependencyProperty),
+        new PropertyMetadata(new CornerRadius(CornerRadiusDependencyProperty.DefaultCornerRadius)));
+
+    /// <summary>
+    ///     Gets the value of the corner radius <see cref="DependencyProperty" />.
+    /// </summary>
+    /// <param name="element">The element to that the <see cref="DependencyProperty" /> is attached to.</param>
+    /// <returns>The value of the <see cref="DependencyProperty" />.</returns>
+    public static CornerRadius GetCornerRadius(DependencyObject element)
+    {
+        return (CornerRadius) element.GetValue(CornerRadiusDependencyProperty.CornerRadiusProperty);
+    }
+
+    /// <summary>
+    ///     Sets the value of the corner radius <see cref="DependencyProperty" />.
+    /// </summary>
+    /// <param name="element">The element to that the <see cref="DependencyProperty" /> is attached to.</param>
+    /// <param name="value">The new value of the <see cref="DependencyProperty" />.</param>
+    public static void SetCornerRadius(DependencyObject element, CornerRadius value)
+    {
+        element.SetValue(
+            CornerRadiusDependencyProperty.CornerRadiusProperty,
+            value);
+    }
+}
