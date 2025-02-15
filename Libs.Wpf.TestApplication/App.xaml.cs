c6e442c16fefe02e6a0d3a23d554da351f8b1b70
MichaelDiers <md@sreid.de>
2025-02-15 23:27:34 +0100

    add wpf test app, reformat code

---
+++ Libs.Wpf.TestApplication/App.xaml.cs
@@ -0,0 +1,6 @@
+﻿namespace Libs.Wpf.TestApplication;
+
+/// <summary>
+///     Interaction logic for App.xaml
+/// </summary>
+public partial class App;
---
+++ Libs.Wpf.TestApplication/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Libs.Wpf.TestApplication;
+
+using System.Windows;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        this.InitializeComponent();
+    }
+}
