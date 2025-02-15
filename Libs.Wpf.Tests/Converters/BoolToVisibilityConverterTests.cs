93ecb8555b4dc03d2c6fb7e21f7a0944579a110c
MichaelDiers <md@sreid.de>
2025-02-15 12:52:42 +0100

    add value converters and tests

---
+++ Libs.Wpf.Tests/Converters/BoolToVisibilityConverterTests.cs
@@ -0,0 +1,46 @@
+﻿namespace Libs.Wpf.Tests.Converters;
+
+using System.Globalization;
+using System.Windows;
+using Libs.Wpf.Converters;
+
+/// <summary>
+///     Tests of <see cref="BoolToVisibilityConverter" />.
+/// </summary>
+public class BoolToVisibilityConverterTests
+{
+    [Theory]
+    [InlineData(
+        null,
+        Visibility.Collapsed)]
+    [InlineData(
+        "string",
+        Visibility.Collapsed)]
+    [InlineData(
+        false,
+        Visibility.Collapsed)]
+    [InlineData(
+        true,
+        Visibility.Visible)]
+    public void Convert(object? value, Visibility expectedVisibility)
+    {
+        Assert.Equal(
+            expectedVisibility,
+            new BoolToVisibilityConverter().Convert(
+                value,
+                typeof(Visibility),
+                null,
+                CultureInfo.CurrentCulture));
+    }
+
+    [Fact]
+    public void ConvertBack_ThrowsNotImplementedException()
+    {
+        Assert.Throws<NotImplementedException>(
+            () => new BoolToVisibilityConverter().ConvertBack(
+                null,
+                typeof(int),
+                null,
+                CultureInfo.CurrentCulture));
+    }
+}
---
+++ Libs.Wpf.Tests/Converters/NullToVisibilityConverterTests.cs
@@ -0,0 +1,43 @@
+﻿namespace Libs.Wpf.Tests.Converters;
+
+using System.Globalization;
+using System.Windows;
+using Libs.Wpf.Converters;
+
+/// <summary>
+///     Tests of <see cref="NullToVisibilityConverter" />.
+/// </summary>
+public class NullToVisibilityConverterTests
+{
+    [Theory]
+    [InlineData(
+        null,
+        Visibility.Collapsed)]
+    [InlineData(
+        "not null",
+        Visibility.Visible)]
+    [InlineData(
+        false,
+        Visibility.Visible)]
+    public void Convert(object? value, Visibility expectedVisibility)
+    {
+        Assert.Equal(
+            expectedVisibility,
+            new NullToVisibilityConverter().Convert(
+                value,
+                typeof(Visibility),
+                null,
+                CultureInfo.CurrentCulture));
+    }
+
+    [Fact]
+    public void ConvertBack_ThrowsNotImplementedException()
+    {
+        Assert.Throws<NotImplementedException>(
+            () => new NullToVisibilityConverter().ConvertBack(
+                null,
+                typeof(int),
+                null,
+                CultureInfo.CurrentCulture));
+    }
+}
