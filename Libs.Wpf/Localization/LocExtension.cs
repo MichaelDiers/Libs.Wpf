df1b89ab455dccef8f3865541da1dceade207c23
MichaelDiers <md@sreid.de>
2025-02-18 01:35:55 +0100

    add localization support

---
+++ Libs.Wpf/Localization/LocExtension.cs
@@ -0,0 +1,98 @@
+﻿// Modified version of https://github.com/Jinjinov/wpf-localization-multiple-resource-resx-one-language/tree/master
+
+namespace Libs.Wpf.Localization;
+
+using System.Resources;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+/// <summary>
+///     Localization extension for multi-language applications.
+/// </summary>
+/// <param name="resourceKey">A key from a resource file.</param>
+public class LocExtension(string resourceKey) : MarkupExtension
+{
+    /// <summary>
+    ///     Gets the resource key.
+    /// </summary>
+    public string ResourceKey { get; } = resourceKey;
+
+    /// <summary>
+    ///     Find a matching <see cref="Translation" /> <see cref="DependencyObject" /> and set the binding.
+    /// </summary>
+    /// <param name="serviceProvider">Object that can provide services for the markup extension.</param>
+    /// <returns>The object to set on this property.</returns>
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        // targetObject is the control that is using the LocExtension
+        var targetObject = (serviceProvider as IProvideValueTarget)?.TargetObject;
+
+        if (targetObject?.GetType().Name == "SharedDp") // is extension used in a control template?
+        {
+            return targetObject; // required for template re-binding
+        }
+
+        var baseName = string.Empty;
+        var current = targetObject as DependencyObject;
+
+        while (string.IsNullOrWhiteSpace(baseName) && current is not null)
+        {
+            baseName = LocExtension.GetResourceManager(current)?.BaseName ?? string.Empty;
+
+            if (string.IsNullOrEmpty(baseName) && current is FrameworkElement frameworkElement) // template re-binding
+            {
+                baseName = LocExtension.GetResourceManager(frameworkElement.TemplatedParent)?.BaseName ?? string.Empty;
+            }
+
+            current = VisualTreeHelper.GetParent(current) ?? (current as FrameworkElement)?.Parent;
+        }
+
+        var binding = new Binding
+        {
+            Mode = BindingMode.OneWay,
+            Path = new PropertyPath($"[{baseName}.{this.ResourceKey}]"),
+            Source = TranslationSource.Instance,
+            FallbackValue = this.ResourceKey
+        };
+
+        return binding.ProvideValue(serviceProvider);
+    }
+
+    /// <summary>
+    ///     Try to find a <see cref="Translation.ResourceManagerProperty" /> on the given <paramref name="control" />.
+    /// </summary>
+    /// <param name="control">
+    ///     The <paramref name="control" /> is checked for the
+    ///     <see cref="Translation.ResourceManagerProperty" />.
+    /// </param>
+    /// <returns>
+    ///     The found matching <see cref="ResourceManager" /> or null if
+    ///     <see cref="Translation.ResourceManagerProperty" /> is not set.
+    /// </returns>
+    private static ResourceManager? GetResourceManager(object? control)
+    {
+        if (control is not DependencyObject dependencyObject)
+        {
+            return null;
+        }
+
+        var localValue = dependencyObject.ReadLocalValue(Translation.ResourceManagerProperty);
+
+        // does this control have a "Translation.ResourceManager" attached property with a set value?
+        if (localValue == DependencyProperty.UnsetValue)
+        {
+            return null;
+        }
+
+        if (localValue is not ResourceManager resourceManager)
+        {
+            return null;
+        }
+
+        TranslationSource.Instance.AddResourceManager(resourceManager);
+
+        return resourceManager;
+    }
+}
---
+++ Libs.Wpf/Localization/TranslationSource.cs
@@ -0,0 +1,107 @@
+﻿// Modified version of https://github.com/Jinjinov/wpf-localization-multiple-resource-resx-one-language/tree/master
+
+namespace Libs.Wpf.Localization;
+
+using System.ComponentModel;
+using System.Globalization;
+using System.Resources;
+
+/// <summary>
+///     Handles localization and the <see cref="ResourceManager" />s of the applications.
+/// </summary>
+internal class TranslationSource : INotifyPropertyChanged
+{
+    /// <summary>
+    ///     The <see cref="Dictionary{TKey,TValue}" /> of added <see cref="ResourceManager" />-
+    /// </summary>
+    private readonly Dictionary<string, ResourceManager> resourceManagerDictionary = new();
+
+    /// <summary>
+    ///     The current <see cref="CompareInfo" />.
+    /// </summary>
+    private CultureInfo currentCulture = CultureInfo.InstalledUICulture;
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="TranslationSource" /> class.
+    /// </summary>
+    private TranslationSource()
+    {
+    }
+
+    /// <summary>
+    ///     Gets or sets the current <see cref="CompareInfo" />.
+    /// </summary>
+    public CultureInfo CurrentCulture
+    {
+        get => this.currentCulture;
+        set
+        {
+            if (this.currentCulture.Equals(value))
+            {
+                return;
+            }
+
+            this.currentCulture = value;
+            // string.Empty/null indicates that all properties have changed
+            this.PropertyChanged?.Invoke(
+                this,
+                new PropertyChangedEventArgs(string.Empty));
+        }
+    }
+
+    /// <summary>
+    ///     A singleton that handles the localization of the application.
+    /// </summary>
+    public static TranslationSource Instance { get; } = new();
+
+    /// <summary>
+    ///     An indexer to get a translation.
+    /// </summary>
+    /// <param name="key">The resource key.</param>
+    /// <returns>The translated key.</returns>
+    public string this[string key]
+    {
+        get
+        {
+            var (resourceName, resourceKey) = TranslationSource.SplitName(key);
+            if (this.resourceManagerDictionary.TryGetValue(
+                    resourceName,
+                    out var translation))
+            {
+                return translation.GetString(
+                           resourceKey,
+                           this.currentCulture) ??
+                       key;
+            }
+
+            return key;
+        }
+    }
+
+    /// <summary>
+    ///     Adds an additional <see cref="ResourceManager" />.
+    /// </summary>
+    /// <param name="resourceManager">The <see cref="ResourceManager" /> to add.</param>
+    public void AddResourceManager(ResourceManager resourceManager)
+    {
+        this.resourceManagerDictionary.TryAdd(
+            resourceManager.BaseName,
+            resourceManager);
+    }
+
+    /// <summary>
+    ///     WPF bindings register PropertyChanged event if the object supports it and update themselves when it is raised
+    /// </summary>
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    /// <summary>
+    ///     Split the <paramref name="name" /> into resource name and resource key.
+    /// </summary>
+    /// <param name="name">The name to split.</param>
+    /// <returns>The split result.</returns>
+    private static (string resourceName, string resourceKey) SplitName(string name)
+    {
+        var index = name.LastIndexOf('.');
+        return (name[..index], name[(index + 1)..]);
+    }
+}
