0516d7db1e714987bc060f5c6b8077b7e23fd242
MichaelDiers <md@sreid.de>
2025-03-28 23:31:50 +0100

    add docs

--- Libs.Wpf/Behaviours/Password/PasswordBehavior.cs
+++ Libs.Wpf/Behaviours/Password/PasswordBehavior.cs
@@ -4,6 +4,10 @@ using System.Windows;
 using System.Windows.Controls;
 using Microsoft.Xaml.Behaviors;
 
+/// <summary>
+///     A behavior that adds a binding functionality for a <see cref="PasswordBox" />.
+/// </summary>
+/// <seealso cref="SecurePasswordDependencyProperty" />
 public class PasswordBehavior : Behavior<PasswordBox>
 {
     /// <summary>
@@ -28,6 +32,11 @@ public class PasswordBehavior : Behavior<PasswordBox>
         base.OnDetaching();
     }
 
+    /// <summary>
+    ///     Handles the change of the password.
+    /// </summary>
+    /// <param name="sender">The object that raised the event.</param>
+    /// <param name="e">The event args.</param>
     private void OnSecurePasswordChanged(object sender, RoutedEventArgs e)
     {
         SecurePasswordDependencyProperty.SetSecureString(
